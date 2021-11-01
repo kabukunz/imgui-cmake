2b3cb7c02bc5ae20d90ae885adf66630d02bb80f
kabukunz <kabukunz@gmail.com>
2021-11-01 18:19:23 +0100

    removed forced install dir

--- build.bat
+++ build.bat
@@ -3,6 +3,9 @@ del /S /Q build
 cmake -S . -B build -G Ninja ^
 -DCMAKE_BUILD_TYPE=Release ^
 -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE ^
--DIMGUI_WITH_IMPL=OFF
+-DIMGUI_WITH_IMPL=OFF ^
+-DCMAKE_INSTALL_PREFIX=".\prebuilt"
 
 cmake --build build
+
+cmake --build build --target install
